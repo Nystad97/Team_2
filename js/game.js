3deeff426dbef06b67dfdf12a8086e7c33d687ee
AxelLoen <55088325+AxelLoen@users.noreply.github.com>
2019-10-23 16:25:16 +0200

    Om axel + tilfeldigbrett halvveis ferdig

--- js/game.js
+++ js/game.js
@@ -1,11 +1,16 @@
 //Kilde: https://github.com/maoesx/Sudoku-/tree/master/Sudoku //
 let time = 300;
 let teller = 0;
+let tilfeldigtall = Math.floor((Math.random()*2)+1)
+console.log(tilfeldigtall);
+
+
 
 //Timer//
 function startTimer(){
     if (time === 300){
         timeoutFunction()
+        
     }
 }
 
@@ -24,7 +29,7 @@ var Sudoku = {
 
     //-------------------------------GAME DATA---------------------------------
     //the Sudoku game data, game can be different if the data changes
-    matrix:[  [[7],[2],[],[9],[4],[5],[],[3],[]],
+    matrix1:[  [[7],[2],[],[9],[4],[5],[],[3],[]],
         [[],[3],[9],[2],[],[6],[],[],[4]],
         [[1],[5],[],[7],[3],[8],[6],[9],[2]],
         [[6],[4],[7],[1],[],[3],[],[2],[]],
@@ -34,19 +39,33 @@ var Sudoku = {
         [[5],[7],[],[3],[2],[],[8],[6],[9]],
         [[],[],[8],[5],[7],[9],[3],[4],[]]
     ],
+    matrix2:[  
+        [[],[],[5],[3],[],[],[],[],[]],
+        [[8],[],[],[],[],[],[],[2],[]],
+        [[],[7],[],[],[1],[],[5],[],[]],
+        [[4],[],[],[],[],[5],[3],[],[]],
+        [[],[1],[],[],[7],[],[],[],[6]],
+        [[],[],[3],[2],[],[],[],[8],[]],
+        [[],[6],[],[5],[],[],[],[],[9]],
+        [[],[],[4],[],[],[],[],[3],[]],
+        [[],[],[],[],[],[9],[7],[],[]]
+],
 
+    
     //-----------------------------START FUNCTION-------------------------------
     //render game board and input Sudoku numbers
     start: function(){
 
         //render game board
+        //Matrix1 funker ikke helt enda. 
+        if(tilfeldigtall == 1){
         for (var i = 0; i < 9; i++) {
             var row = $('<tr></tr>');
             for(var j = 0; j < 9; j++){
                 var sBlock = $('<td class="sBox edit"></td>');
-                sBlock.attr('id','Block'+'_'+ i + '_' + j).text(Sudoku.matrix[i][j]);   //store the block location in the id
+                sBlock.attr('id','Block'+'_'+ i + '_' + j).text(Sudoku.matrix2[i][j]);   //store the block location in the id
                 row.append(sBlock);
-                if(Sudoku.matrix[i][j] != ''){  //the number in block with edit class can be changed
+                if(Sudoku.matrix2[i][j] != ''){  //the number in block with edit class can be changed
                     sBlock.removeClass('edit');
                 }
                 var groups = Math.floor(Math.sqrt(9));  //use different color to distinguish different groups
@@ -59,6 +78,30 @@ var Sudoku = {
                 }
             $('#sTable').append(row);
             }
+        }
+        }
+        else if(tilfeldigtall == 2){
+            for (var i = 0; i < 9; i++) {
+                var row = $('<tr></tr>');
+                for(var j = 0; j < 9; j++){
+                    var sBlock = $('<td class="sBox edit"></td>');
+                    sBlock.attr('id','Block'+'_'+ i + '_' + j).text(Sudoku.matrix2[i][j]);   //store the block location in the id
+                    row.append(sBlock);
+                    if(Sudoku.matrix2[i][j] != ''){  //the number in block with edit class can be changed
+                        sBlock.removeClass('edit');
+                    }
+                    var groups = Math.floor(Math.sqrt(9));  //use different color to distinguish different groups
+                    var gA = Math.floor(i / groups);
+                    var gB = Math.floor(j / groups);
+                    if (gA % 2 == gB % 2) {
+                        sBlock.addClass('sGroup');
+                    }else{
+                        sBlock.addClass('sGroup2');
+                    }
+                $('#sTable').append(row);
+                }
+            }
+            
         }
     },
 
@@ -80,7 +123,7 @@ var Sudoku = {
             var location = $('.selectActive').attr('id').split('_');    //analyze the id to get the location of the block selected
             var thisRow = parseInt(location[1]);    //the x-axis of the block
             var thisCol = parseInt(location[2]);    //the y-axis of the block
-            Sudoku.matrix[thisRow][thisCol] = parseInt(thisInput);  //update the input to the data matrix
+            Sudoku.matrix2[thisRow][thisCol] = parseInt(thisInput);  //update the input to the data matrix
 
             //check the number input
             $('.sWrong').removeClass('sWrong');
@@ -103,20 +146,20 @@ var Sudoku = {
     //compare numbers on the board to find potential mistake
     compare : function(){
         teller++; //øker oversikten over antall trekk som er foretatt//
-        var matrix = Sudoku.matrix;
+        var matrix2 = Sudoku.matrix2;
          for(var i=0; i<9; i++){
              for(var j=0; j<9; j++){
                  for(var h=0; h<9; h++){
                      if(
-                         (matrix[i][j] == matrix[i][h] && j != h)   //valid rows in Sudoku rules
-                         || (matrix[i][j] == matrix[h][j] && i != h)    //valid cols in Sudoku rules
+                         (matrix2[i][j] == matrix2[i][h] && j != h)   //valid rows in Sudoku rules
+                         || (matrix2[i][j] == matrix2[h][j] && i != h)    //valid cols in Sudoku rules
                        ){
                          $('#Block_'+i+'_'+j).addClass('sWrong');  //if the number is wrong, show it with a red background
                      };
                  for(var k = 0; k < 3; k++) //valid groups in Sudoku rules
                      for(var l = 0; l < 3; l++)
                          if(
-                            (matrix[i][j] == matrix[parseInt(i / 3) * 3 + k][parseInt(j / 3) * 3 + l])
+                            (matrix2[i][j] == matrix2[parseInt(i / 3) * 3 + k][parseInt(j / 3) * 3 + l])
                             && (!(i == parseInt(i / 3) * 3+k && j == parseInt(j / 3) * 3+l))
                             ){
                                  $('#Block_'+i+'_'+j).addClass('sWrong');
@@ -131,3 +174,4 @@ $(document).ready(function(){
     Sudoku.start();
     Sudoku.play();
 });
+
